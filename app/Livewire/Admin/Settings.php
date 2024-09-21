28244852d3a7d4492099c7dd389052426110a3f8
Calum Chamberlain <calum@appoly.co.uk>
2024-09-21 08:27:54 +0100

    remove address

--- app/Livewire/Admin/Settings.php
+++ app/Livewire/Admin/Settings.php
@@ -11,16 +11,6 @@ class Settings extends Component
 
     public $weddingVenue;
 
-    public $addressLineOne;
-
-    public $addressLineTwo;
-
-    public $town;
-
-    public $county;
-
-    public $postcode;
-
     public function render()
     {
         return view('livewire.admin.settings');
@@ -30,21 +20,6 @@ class Settings extends Component
     {
         $this->weddingDate = Setting::find('wedding_date');
         $this->weddingVenue = Setting::find('wedding_venue');
-        $address = Setting::find('wedding_venue_address');
-        if (! $address) {
-            $address = [
-                'address_1' => '',
-                'address_2' => '',
-                'town' => '',
-                'county' => '',
-                'postcode' => '',
-            ];
-        }
-        $this->addressLineOne = $address['address_1'];
-        $this->addressLineTwo = $address['address_2'];
-        $this->town = $address['town'];
-        $this->county = $address['county'];
-        $this->postcode = $address['postcode'];
     }
 
     public function updateWeddingSettings()
@@ -52,11 +27,6 @@ class Settings extends Component
         $this->validate([
             'weddingDate' => ['required', 'date'],
             'weddingVenue' => ['required'],
-            'addressLineOne' => ['required'],
-            'addressLineTwo' => ['nullable'],
-            'town' => ['required'],
-            'county' => ['required'],
-            'postcode' => ['required'],
         ]);
 
         Setting::updateOrCreate([
@@ -70,19 +40,6 @@ class Settings extends Component
 
         ], [
             'value' => $this->weddingVenue,
-        ]);
-
-        Setting::updateOrCreate([
-            'key' => 'wedding_venue_address',
-
-        ], [
-            'value' => [
-                'address_1' => $this->addressLineOne,
-                'address_2' => $this->addressLineTwo,
-                'town' => $this->town,
-                'county' => $this->county,
-                'postcode' => $this->postcode,
-            ],
         ]);
 
         $this->dispatch('toast', message: 'Wedding settings updated successfully.', data: ['position' => 'top-right', 'type' => 'success']);
